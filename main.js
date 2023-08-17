56c00d78e2fe031870243a3c3103c3b04bfa7f9d
Adebisi Adebambo <136770047+debxam@users.noreply.github.com>
2023-08-16 21:30:50 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function toggleNavbar() {
+  var navbar = document.getElementById("myNavbar");
+  var icon = document.querySelector(".icon");
+  
+  if (navbar.classList.contains("responsive")) {
+    navbar.classList.remove("responsive");
+    icon.classList.remove("open");
+  } else {
+    navbar.classList.add("responsive");
+    icon.classList.add("open");
+  }
+}
